1b48d35462585284d078cb04cfa6c8549b366882
ksh <hyun031916@gmail.com>
2020-09-05 02:08:05 +0900

    #5 완성된 Java 코드

--- app/src/main/java/com/cookandroid/simplecalculator/MainActivity.java
+++ app/src/main/java/com/cookandroid/simplecalculator/MainActivity.java
@@ -25,9 +25,9 @@ public class MainActivity extends AppCompatActivity {
         edit1 = (EditText)findViewById(R.id.Edit1);
         edit2 = (EditText)findViewById(R.id.Edit2);
         btnAdd = (Button)findViewById(R.id.BtnAdd);
-//        btnSub = (Button)findViewById(R.id.BtnSub);
-//        btnMul = (Button)findViewById(R.id.BtnMul);
-//        btnDiv = (Button)findViewById(R.id.BtnDiv);
+        btnSub = (Button)findViewById(R.id.BtnSub);
+        btnMul = (Button)findViewById(R.id.BtnMul);
+        btnDiv = (Button)findViewById(R.id.BtnDiv);
 
         textResult = (TextView)findViewById(R.id.TextResult);
 
@@ -37,6 +37,39 @@ public class MainActivity extends AppCompatActivity {
                 num1 = edit1.getText().toString();
                 num2 = edit2.getText().toString();
                 result = Integer.parseInt(num1)+Integer.parseInt(num2);
+                textResult.setText("계산 결과 : "+result.toString());
+                return false;
+            }
+        });
+
+        btnSub.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1)-Integer.parseInt(num2);
+                textResult.setText("계산 결과 : "+result.toString());
+                return false;
+            }
+        });
+
+        btnMul.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1)*Integer.parseInt(num2);
+                textResult.setText("계산 결과 : "+result.toString());
+                return false;
+            }
+        });
+
+        btnDiv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1)/Integer.parseInt(num2);
                 textResult.setText("계산 결과 : "+result.toString());
                 return false;
             }
