fb2565cc1f3d6876979cd9d655ed73bbc6350eef
ksh <hyun031916@gmail.com>
2020-09-05 01:53:58 +0900

    #2 Java 코드 1

--- app/src/main/java/com/cookandroid/simplecalculator/MainActivity.java
+++ app/src/main/java/com/cookandroid/simplecalculator/MainActivity.java
@@ -3,12 +3,21 @@ package com.cookandroid.simplecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
